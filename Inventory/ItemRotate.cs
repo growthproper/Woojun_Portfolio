f4094c38399bae1b75ac2eb6ed72ed27978a6de1
growthproper <89292360+growthproper@users.noreply.github.com>
2023-02-12 21:05:51 +0900

    Update ItemRotate.cs

--- Inventory/ItemRotate.cs
+++ Inventory/ItemRotate.cs
@@ -3,14 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemRotate : MonoBehaviour
-{
-    
-    void Start()
-    {
-        
-    }
-
-    
+{   
     void Update()
     {
         transform.Rotate(Vector3.up * Time.deltaTime, 1f);
