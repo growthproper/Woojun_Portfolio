d45ba61fb509277680a73354977d6ecc1a986a8a
growthproper <89292360+growthproper@users.noreply.github.com>
2023-02-12 20:52:35 +0900

    Update SetManager.cs

--- Manager/SetManager.cs
+++ Manager/SetManager.cs
@@ -14,26 +14,17 @@ public class SetManager : MonoBehaviour
     {
         PlayerObj = GameObject.FindGameObjectWithTag("Player");
         CameraObj = GetComponent<GameObject>();
-        PlayerPos = PlayerObj.transform;
-
-
-        
-        InstanceManager.instance.Initialize();
-        
+        PlayerPos = PlayerObj.transform;        
+        InstanceManager.instance.Initialize();        
        if(SceneManager.GetActiveScene().name == "Dungeon")
         {
             InstanceManager.instance.CreateMonster("Monster");
             InstanceManager.instance.CreateBossMonster();
         }
-
         //플레이어 오브젝트에 카메라 붙이기
         //플레이어 게임 오브젝트가 씬에 없다면 Player 태그의 게임 오브젝트를 찾아 PlayerObj 변수에 저장
         if (PlayerObj != null) // 플레이어 게임 오브젝트가 씬에 있다면 카메라 프리팹을 생성하여 freeLookCamera의 Follow, LookAt을 플레이어로 지정
-        {
-            //if (SceneManager.GetActiveScene().name == "Dungeon")
-            //{
-            //    InstanceManager.instance.CreateMonster("Monster");
-            //}
+        {            
             InstanceManager.instance.CreateCamera("Camera");
             CameraController.freeLookCameraComponent.Follow = PlayerPos;
             CameraController.freeLookCameraComponent.LookAt = PlayerPos;
